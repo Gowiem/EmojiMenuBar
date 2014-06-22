7460fec2e7c2f3fbf7c198dda3eb2aad1e131838
Gowiem <gowie.matt@gmail.com>
2014-06-22 16:18:00 -0400

    Adds status bar item w/ popover

--- EmojiMenuBar/AppDelegate.swift
+++ EmojiMenuBar/AppDelegate.swift
@@ -8,19 +8,45 @@
 
 import Cocoa
 
-class AppDelegate: NSObject, NSApplicationDelegate {
-                            
-    @IBOutlet var window: NSWindow
+class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
+    
+    @IBOutlet var statusMenu: NSMenu
+    var statusItem: NSStatusItem?
+    @IBOutlet var mainPopover: NSPopover
 
 
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
-        // Insert code here to initialize your application
+        let bar = NSStatusBar.systemStatusBar()
+        
+        statusItem = bar.statusItemWithLength(CGFloat(NSVariableStatusItemLength))
+        statusItem!.title = nil
+        statusItem!.menu = statusMenu
+        statusItem!.highlightMode = true
+        statusItem!.action = "showPopover:"
+        
+        let icon = NSImage(named: "Icon")
+        icon.size = NSSize(width: 16, height: 16)
+        icon.setTemplate(true)
+        statusItem!.image = icon
     }
-
-    func applicationWillTerminate(aNotification: NSNotification?) {
-        // Insert code here to tear down your application
+    
+    func menuNeedsUpdate(menu: NSMenu!) {
+        if (menu != self.statusMenu) {
+            return
+        }
+        
+        menu.removeAllItems()
+        menu.addItem(NSMenuItem(title: "Quit", action: "statusMenuItemQuit_Action:", keyEquivalent: ""))
     }
-
-
+    
+    func statusMenuItemQuit_Action(sender: NSMenuItem) {
+        NSApp.terminate(self)
+    }
+    
+    func showPopover(sender: AnyObject) {
+        // Need to downcast our sender as a view since it's a private class (NSStatusBarButton)
+        let statusBarButton = sender as NSView
+        mainPopover.showRelativeToRect(NSZeroRect, ofView: statusBarButton, preferredEdge: NSRectEdge.min)
+    }
 }
 
---
+++ EmojiMenuBar/EMPopoverViewController.swift
@@ -0,0 +1,22 @@
+//
+//  EMPopoverViewController.swift
+//  EmojiMenuBar
+//
+//  Created by Matt Gowie on 6/21/14.
+//  Copyright (c) 2014 Artisan. All rights reserved.
+//
+
+import Cocoa
+
+class EMPopoverViewController: NSViewController {
+
+    init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        // Initialization code here.
+    }
+    
+    init(coder aDecoder: NSCoder!) {
+        super.init(coder: aDecoder)
+    }
+
+}
